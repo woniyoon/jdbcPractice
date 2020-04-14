9fa2232bb9143834fcb3bcdaa92aabe8758a703f
woniyoon <gellifisch@gmail.com>
2020-04-14 16:06:33 +0900

    Add editPost method in Controller class

--- src/jdbc/day04/board/Controller.java
+++ src/jdbc/day04/board/Controller.java
@@ -223,19 +223,15 @@ public class Controller {
 					} else {
 						System.out.println(">> 수정실패!! <<");
 					}
-					
 				} else {
 					System.out.println(">> 글암호가 올바르지 않습니다 << ");
 				}
-				
 			} else {
 				System.out.println("\n>> 다른 사용자의 글은 수정불가 합니다!! << ");
 			}
-			
 		} else {
-			System.out.println("\n>>> 존재하지 않는 글번호입니다! <<< ");
+			System.out.println("\n>>> 존재하지 않는 글번호입니다! <<< ");	
 		}
-		
 	}
 
 	// 자원반납
